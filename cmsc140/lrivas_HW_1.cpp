976fc1fc1dee8a1b8806ae6a8ad950b344c416ab
Luis Rivas Lopez <luisarl355@gmail.com>
2022-09-19 00:05:38 -0400

    Add content to print out

--- cmsc140/lrivas_HW_1.cpp
+++ cmsc140/lrivas_HW_1.cpp
@@ -1,10 +1,23 @@
+/*
+ * Class: CMSC140 CRN 25369
+ * Instructor: Dr.Grigory
+ * Classwork 1
+ * Computer/Platform/Compiler:
+ * Description: A simple C++ program to display details about myself
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any student.
+   Print your Name here: Luis Rivas Lopez
+
+*/
+
 #include <iostream>
 
 using namespace std;
 
 int main() 
 {
-	cout << "Hello World" << endl;
+	cout << "Hello CMSC140 CRN 25369 class! \nMy name is Luis Rivas Lopez. \nMy major is Computer Science. Nice to see you! \nToday is September 19th" << endl;
 
 	system("pause");
 	return 0;
