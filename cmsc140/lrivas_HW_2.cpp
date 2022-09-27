90a023e9d4b3e62a12b772acb1e6bc5843c4fab4
Luis Rivas Lopez <76708966+Lrivaslo5@users.noreply.github.com>
2022-09-26 23:15:04 -0400

    Added HW2

---
+++ cmsc140/lrivas_HW_2.cpp
@@ -0,0 +1,24 @@
+/*
+ * Class: CMSC140 CRN 25369
+ * Instructor: Dr.Grigory
+ * Homework 2
+ * Computer/Platform/Compiler:
+ * Description: A simple C++ program to display details about myself
+ * I pledge that I have completed the programming assignment independently.
+   I have not copied the code from a student or any source.
+   I have not given my code to any student.
+   Print your Name here: Luis Rivas Lopez
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	cout << "Luis Rivas Lopez\n7153 Lingenor Road, Frederick,MD, 21701\n2405517993\nComputer Science\n" << endl;
+
+	system("pause");
+	return 0;
+}
